edf549c49c183e753058f75c54291c4e750292d6
Yuliy97 <yuliyliu97@gmail.com>
2018-06-18 16:58:28 -0400

    initial get recurring order

--- src/main/java/io/swagger/api/RecurringApiController.java
+++ src/main/java/io/swagger/api/RecurringApiController.java
@@ -5,14 +5,20 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.model.RecurringOrder;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
+import io.swagger.services.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.List;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-06-18T11:50:46.970-04:00")
 
@@ -25,10 +31,14 @@ public class RecurringApiController implements RecurringApi {
 
   private final HttpServletRequest request;
 
+  @Autowired
+  private final OrderService service;
+
   @org.springframework.beans.factory.annotation.Autowired
-  public RecurringApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+  public RecurringApiController(ObjectMapper objectMapper, HttpServletRequest request, OrderService orderService) {
     this.objectMapper = objectMapper;
     this.request = request;
+    this.service = orderService;
   }
 
   public ResponseEntity<Void> addRecurring(@ApiParam(value = "New recurring order", required = true) @Valid @RequestBody RecurringOrder body) {
@@ -46,7 +56,13 @@ public class RecurringApiController implements RecurringApi {
   public ResponseEntity<Void> retrieveRecurring(
       @ApiParam(value = "x-tenant to retrieve orders for", required = true) @PathVariable("target") String target) {
     String accept = request.getHeader("Accept");
-    return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+    List<RecurringOrder> toReturn = null;
+    try {
+      toReturn = service.getAllRecurringOrders();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return new ResponseEntity<Void>((MultiValueMap<String, String>) toReturn, HttpStatus.OK);
   }
 
   public ResponseEntity<Void> updateRecurring(@ApiParam(value = "Update to recurring order", required = true) @Valid @RequestBody RecurringOrder body,
