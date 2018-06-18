7917ec5e7fc4d76b4a965f3e24af029a8d49609a
David Sargent <david_sargent@ultimatesoftware.com>
2018-06-18 17:20:12 -0400

    Allows Spring to actually boot

--- src/main/java/io/swagger/services/OrderService.java
+++ src/main/java/io/swagger/services/OrderService.java
@@ -8,13 +8,15 @@ import io.swagger.repo.RecurringRepo;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OrderService {
   private final OrderRepo orderRepository;
   private final RecurringRepo recurringRepository;
 
-  @Autowired
-  public OrderService(OrderRepo orderRepository, RecurringRepo recurringRepository) {
+
+  public OrderService(@Autowired OrderRepo orderRepository, @Autowired RecurringRepo recurringRepository) {
     this.orderRepository = orderRepository;
     this.recurringRepository = recurringRepository;
   }
