b81011a468c88dbb4c8f68861496c0765106f81f
Yuliy97 <yuliyliu97@gmail.com>
2018-06-19 19:47:24 -0400

    freaking compareTo

--- src/main/java/io/swagger/services/UltiOrderService.java
+++ src/main/java/io/swagger/services/UltiOrderService.java
@@ -5,12 +5,15 @@ import io.swagger.model.OneTimeOrder;
 import io.swagger.model.RecurringOrder;
 import io.swagger.repo.OrderRepo;
 import io.swagger.repo.RecurringRepo;
+
+import java.nio.file.InvalidPathException;
 import java.rmi.UnexpectedException;
 import java.util.List;
 import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.InvalidPropertyException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.aggregation.BooleanOperators;
 import org.springframework.stereotype.Service;
@@ -62,7 +65,7 @@ public class UltiOrderService {
 
   public void addRecurringOrder(RecurringOrder body) {
       RecurringOrder order = recurringRepository.findById(body.getId());
-      if (order == null) {
+      if (order != null) {
         recurringRepository.delete(order.getId());
       }
       recurringRepository.save(body);
@@ -123,9 +126,13 @@ public class UltiOrderService {
       }
     }
   }
-  public void updateRecurringOrder(RecurringOrder body) throws NotFoundException {
-      if (recurringRepository.findById(body.getId()) == null) {
+  public void updateRecurringOrder(RecurringOrder body, UUID target) throws NotFoundException {
+      RecurringOrder oldOrder = recurringRepository.findById(target);
+      if (oldOrder == null) {
         throw new NotFoundException(404, "No Such Order");
+      }
+      if (body.getId().compareTo(target) != 0) {
+        throw new NotFoundException(401, "Path does not match id field");
       }
       recurringRepository.save(body);
   }
