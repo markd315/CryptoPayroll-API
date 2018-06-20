da9ce2a386233446883864334731a7018750bb0e
markda <mark_davis@ultimatesoftware.com>
2018-06-20 18:25:27 -0400

    Revert "Renamed packages and classes"
    This reverts commit 74bfe4e2cfebee6961b326ed834a580db8c5f699.

---
+++ src/main/java/io/swagger/model/Order.java
@@ -0,0 +1,156 @@
+package io.swagger.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.UUID;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+public interface Order {
+  Order id(UUID id);
+
+  /**
+   * Overwritten by server
+   * @return id
+   **/
+  @ApiModelProperty(value = "Overwritten by server")
+
+  @Valid
+  UUID getId();
+
+  void setId(UUID id);
+
+  Order currency(OneTimeOrder.CurrencyEnum currency);
+
+  /**
+   * Three-digit trade code
+   * @return currency
+   **/
+  @ApiModelProperty(required = true, value = "Three-digit trade code")
+  @NotNull
+  OneTimeOrder.CurrencyEnum getCurrency();
+
+  void setCurrency(OneTimeOrder.CurrencyEnum currency);
+
+  Order quantity(Double quantity);
+
+  /**
+   * Amount of currency to send
+   * @return quantity
+   **/
+  @ApiModelProperty(required = true, value = "Amount of currency to send")
+  @NotNull
+  double getQuantity();
+
+  void setQuantity(Double quantity);
+
+  Order destination(String destination);
+
+  /**
+   * Get destination
+   * @return destination
+   **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+  String getDestination();
+
+  void setDestination(String destination);
+
+  Order destinationType(OneTimeOrder.DestinationTypeEnum destinationType);
+
+  /**
+   * Get destinationType
+   * @return destinationType
+   **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+  OneTimeOrder.DestinationTypeEnum getDestinationType();
+
+  void setDestinationType(OneTimeOrder.DestinationTypeEnum destinationType);
+
+  Order filled(Boolean filled);
+
+  /**
+   * Overwritten by server
+   * @return filled
+   **/
+  @ApiModelProperty(value = "Overwritten by server")
+  boolean isFilled();
+
+  void setFilled(Boolean filled);
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  String toIndentedString(Object o);
+
+  boolean isRecurring();
+
+  /**
+   * Three-digit trade code
+   */
+  enum CurrencyEnum {
+    BTC("BTC"),
+
+    ETH("ETH"),
+
+    LTC("LTC"),
+
+    USD("USD");
+
+    private String value;
+
+    CurrencyEnum(String value) {
+      this.value = value;
+    }
+
+    @Override
+    @JsonValue
+    public String toString() {
+      return String.valueOf(value);
+    }
+
+    @JsonCreator
+    public static CurrencyEnum fromValue(String text) {
+      for (CurrencyEnum b : CurrencyEnum.values()) {
+        if (String.valueOf(b.value).equals(text)) {
+          return b;
+        }
+      }
+      return null;
+    }
+  }
+
+  /**
+   * Gets or Sets destinationType
+   */
+  public enum DestinationTypeEnum {
+    COINBASE("coinbase"),
+
+    WALLET("wallet");
+
+    private String value;
+
+    DestinationTypeEnum(String value) {
+      this.value = value;
+    }
+
+    @Override
+    @JsonValue
+    public String toString() {
+      return String.valueOf(value);
+    }
+
+    @JsonCreator
+    public static DestinationTypeEnum fromValue(String text) {
+      for (DestinationTypeEnum b : DestinationTypeEnum.values()) {
+        if (String.valueOf(b.value).equals(text)) {
+          return b;
+        }
+      }
+      return null;
+    }
+  }
+
+}
---
+++ src/main/java/io/swagger/services/UltiOrderService.java
@@ -0,0 +1,146 @@
+package io.swagger.services;
+
+import io.swagger.api.NotFoundException;
+import io.swagger.model.OneTimeOrder;
+import io.swagger.model.RecurringOrder;
+import io.swagger.repo.OrderRepo;
+import io.swagger.repo.RecurringRepo;
+import java.rmi.UnexpectedException;
+import java.util.List;
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UltiOrderService {
+  @Autowired
+  private OrderRepo orderRepository;
+
+  @Autowired
+  private RecurringRepo recurringRepository;
+
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+  @Autowired
+  public UltiOrderService(OrderRepo orderRepository, RecurringRepo recurringRepository) {
+    this.orderRepository = orderRepository;
+    this.recurringRepository = recurringRepository;
+  }
+
+
+  public OneTimeOrder findOneTimeOrderById(UUID id) throws NotFoundException {
+    OneTimeOrder order = orderRepository.findById(id);
+    if (order == null) {
+      throw new NotFoundException(404, "No such order");
+    }
+    return order;
+  }
+
+  public RecurringOrder findRecurringOrderById(UUID id) throws NotFoundException {
+    RecurringOrder order = recurringRepository.findById(id);
+    if (order == null) {
+      log.info(id.toString());
+      throw new NotFoundException(404, "No such order");
+    }
+    return order;
+  }
+
+  public void addOneTimeOrder(OneTimeOrder body) {
+    try {
+      OneTimeOrder order = orderRepository.findById(body.getId());
+      if (order == null) {
+        throw new NotFoundException(404, "No such order");
+      }
+    } catch (Exception e1) {
+      orderRepository.save(body);
+    }
+  }
+
+  public void addRecurringOrder(RecurringOrder body) {
+    RecurringOrder order = recurringRepository.findById(body.getId());
+    if (order != null) {
+      recurringRepository.delete(order.getId());
+    }
+    recurringRepository.save(body);
+  }
+
+  public void deleteRecurringOrder(UUID id) throws NotFoundException {
+    if (recurringRepository.findById(id) == null) {
+      throw new NotFoundException(404, "No such order");
+    }
+    recurringRepository.delete(id);
+  }
+
+  public RecurringOrder getRecurringOrder(UUID id) throws NotFoundException {
+    RecurringOrder order = recurringRepository.findById(id);
+    if (order == null) {
+      throw new NotFoundException(400, "No such orders");
+    }
+    return order;
+  }
+
+  public List<OneTimeOrder> getAllOneTimeOrders() throws NotFoundException {
+    List<OneTimeOrder> orders = orderRepository.findAll();
+    if (orders == null) {
+      try {
+        throw new NotFoundException(404, "No such orders");
+      } catch (NotFoundException e) {
+        e.printStackTrace();
+      }
+    }
+    return orders;
+  }
+
+  public List<RecurringOrder> getAllRecurringOrders() throws NotFoundException {
+    List<RecurringOrder> orders = recurringRepository.findAll();
+    if (orders == null) {
+      try {
+        throw new NotFoundException(404, "No such orders");
+      } catch (NotFoundException e) {
+        e.printStackTrace();
+      }
+    }
+    return orders;
+  }
+
+  public void wipeAllOneTimeOrders() throws NotFoundException {
+    List<OneTimeOrder> orders = orderRepository.findAll();
+    orderRepository.delete(orders);
+  }
+
+  public void incrementOrResetAllRecurringOrders() {
+    List<RecurringOrder> list = recurringRepository.findAll();
+    for (RecurringOrder recurring : list) {
+      recurring.setCyclesSinceLast(recurring.getCyclesSinceLast() + 1); //Increment our cyclical field.
+      if (recurring.getCyclesSinceLast() == recurring.getCyclePeriod()) {
+        recurring.setCyclesSinceLast(0); //reset the cyclical field
+      }
+      if (recurringRepository.findById(recurring.getId()) != null) //Only save if we can find a version of this already
+      {
+        recurringRepository.save(recurring);
+      } else {
+        try {
+          throw new UnexpectedException("Tried to update entry that does not exist!");
+        } catch (UnexpectedException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
+  public void updateRecurringOrder(RecurringOrder body, UUID target, String code) throws NotFoundException {
+    RecurringOrder oldOrder = recurringRepository.findById(target);
+    if (oldOrder == null) {
+      throw new NotFoundException(404, "No Such Order");
+    }
+    if (body.getId().compareTo(target) != 0) {
+      throw new NotFoundException(401, "Path does not match id field");
+    }
+    if (oldOrder.getCurrency() != body.getCurrency()) {
+      throw new NotFoundException(401, "Currency type cannot be changed");
+    }
+    recurringRepository.save(body);
+  }
+}
