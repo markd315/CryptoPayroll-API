139c5cd6db3f9845e99d8c2a05a6e82951e02063
David Sargent <david_sargent@ultimatesoftware.com>
2018-06-20 10:32:53 -0400

    Autoupdate pricing upon enrollment

--- src/main/resources/static/PAYROLL_files/crypto-pricing.js
+++ src/main/resources/static/PAYROLL_files/crypto-pricing.js
@@ -23,9 +23,35 @@
         ETH: getCurrencyData('ETH')
     };
 
-    $('#per-paycheck-amt').on('input', function() {
-        console.log('moo');
-        let paycheckAmt = Number.parseFloat($(this).val());
-        $('#btc-coin-estimate').html(`<label><strong>$${paycheckAmt / currencies.BTC.price}</strong> Bitcoin at <strong>${currencies.BTC.price}/BTC </strong></label>`)
+    $(document).ready(function() {
+        $('#per-paycheck-amt').on('input', function () {
+            let paycheckAmt = {
+                total: Number.parseFloat($(this).val()),
+                btc: 0,
+                ltc: 0,
+                eth: 0,
+            };
+
+            if (isNaN(paycheckAmt.total)) paycheckAmt.total = 0;
+
+            if ($('#bitcoin-wallet-radio').is(':checked')) {
+                paycheckAmt.btc = paycheckAmt.total;
+            } else if ($('#coinbase-acc-radio').is(':checked')) {
+                let ethPercent = $('#cb-eth-chk').is(':checked') ? Number.parseFloat($('#cb-eth-percentage').val()) : 0;
+                let ltcPercent = $('#cb-ltc-chk').is(':checked') ? Number.parseFloat($('#cb-ltc-percentage').val()) : 0;
+                let btcPercent = $('#cb-btc-chk').is(':checked') ? Number.parseFloat($('#cb-btc-percentage').val()) : 0;
+
+                paycheckAmt.eth = ethPercent * paycheckAmt.total;
+                paycheckAmt.ltc = ltcPercent * paycheckAmt.total;
+                paycheckAmt.btc = btcPercent * paycheckAmt.total;
+            }
+
+            let btcCoinEstimate = Math.trunc(paycheckAmt.btc / currencies.BTC.price * 1000) / 1000;
+            let ethCoinEstimate = Math.trunc(paycheckAmt.eth / currencies.ETH.price * 1000) / 1000;
+            let ltcCoinEstimate = Math.trunc(paycheckAmt.ltc / currencies.LTC.price * 1000) / 1000;
+            $('#btc-coin-estimate').html(`<label><strong>${btcCoinEstimate}</strong> Bitcoin at <strong>${currencies.BTC.price}/BTC </strong></label>`);
+            $('#ltc-coin-estimate').html(`<label><strong>${ltcCoinEstimate}</strong> Litecoin at <strong>${currencies.LTC.price}/LTC </strong></label>`);
+            $('#eth-coin-estimate').html(`<label><strong>${ethCoinEstimate}</strong> Ethereum at <strong>${currencies.ETH.price}/ETH </strong></label>`)
+        });
     });
 })();
